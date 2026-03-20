a448b2675b93152c86844df77d22a451cb7abd9e
ellielyness <140804729+ellielyness@users.noreply.github.com>
2026-03-20 00:22:41 +0000

    added some responsive classes, mostly within structure. Moved the global variables

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 export default function Page() {
     return (
-        <div className="d-flex justify-content-center">
-            <div className="col-6">
-                <h1 className="m-0 text-primary-albedo text-align-center w-50 hover-w-100 hover-bg-secondary">Poos</h1>
+        <div className="d-flex justify-content-center vh-100">
+            <div className="p-0 col-6 d-flex flex-column flex-l-row justify-content-center">
+                <h1 className="my-0 text-primary-albedo text-align-center text-align-xl-left align-content-center align-content-xl-start w-100 bg-primary">Poos</h1>
+                <h1 className="my-0 text-primary-albedo text-align-center text-align-xl-right align-content-center align-content-xl-end w-100 bg-primary">Poos</h1>                
             </div>
         </div>
     )
