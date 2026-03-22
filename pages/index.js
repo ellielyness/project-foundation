63b51cd357ae4989a8a3faaf3871d6b647bd5f62
ellielyness <140804729+ellielyness@users.noreply.github.com>
2026-03-22 23:04:57 +0000

    added some display, positioning and sizing classes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
+import Header from '../components/typography/Header'
+import Grid from '../components/display/Grid'
+
 export default function Page() {
     return (
         <div className="d-flex justify-content-center vh-100">
-            <div className="p-0 col-6 d-flex flex-column flex-l-row justify-content-center">
-                <h1 className="my-0 text-primary-albedo text-align-center text-align-xl-left align-content-center align-content-xl-start w-100 bg-primary">Poos</h1>
-                <h1 className="my-0 text-primary-albedo text-align-center text-align-xl-right align-content-center align-content-xl-end w-100 bg-primary">Poos</h1>                
-            </div>
+            <h1 className="text-secondary-albedo">Test</h1>
+            <Grid />
         </div>
     )
 }
