449629ec50d02f93785b911f178f4e30fdb4e42a
Ellie Lyness <ellie@ellielyness.co.uk>
2026-06-07 08:18:37 +0100

    Added row and container classes for structure

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,22 @@ export default function Page() {
     return (
         <main>
             <Header heading="Lorem Ipsum" subheading="Dolor Sit Amet" />
-            <div id="content" className="container">
-                <div className="bg-secondary-albedo py-1">
-                    <div className="col-10 offset-1">
-                        <p id="intro" className="col-12 font-size-1 font-weight-300 text-transform-uppercase font-width-151 my-1 font-tracking-20 hover-font-weight-500 user-select-none">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-                    </div>
+            <div id="content" className="container-fullwidth">
+                <div className="row bg-secondary-albedo py-1">
+                    <p id="intro" className="col-10 offset-1 font-size-1 font-weight-300 text-transform-uppercase font-width-151 my-1 font-tracking-20 hover-font-weight-500 user-select-none">Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
                 </div>
-                <div className="col-10 offset-1 py-2">
-                    <div className='col-12 col-m-6 v-align-center'>
+                <div className="row py-2">
+                    <div className='col-12 col-m-5 offset-m-1'>
                         <p className='font-size-2 font-leading-1'>An unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting.</p>                    
                         <p>An unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>                    
                         <p>An unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>                    
                     </div>
-                    <div className='col-12 col-m-6'>
+                    <div className='col-12 col-m-5'>
                         <img src="/img/header.jpg" />
                         <p className='caption bg-secondary-albedo text-transform-uppercase font-tracking-20 font-weight-300'><span className='font-weight-500'>Above:</span> This is a test caption to show an example</p>
                     </div>
                 </div>
+                
             </div>
         </main>
     )
